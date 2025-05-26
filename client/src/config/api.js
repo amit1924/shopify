0b8b8165bb61ad3558c106f00719431662379f9a
amit1924 <amit192400@gmail.com>
2025-05-26 17:05:26 +0530

    updated deployed url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -2,6 +2,6 @@ const isLocal = window.location.hostname === "localhost";
 
 const API_URL = isLocal
   ? "http://localhost:3000"
-  : "https://shopify-theta-mauve.vercel.app/";
+  : "https://shopify-theta-mauve.vercel.app";
 
 export default API_URL;
