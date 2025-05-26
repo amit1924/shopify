f78aeb1adc1fe17f38dfa3785abbba40717ad145
amit1924 <amit192400@gmail.com>
2025-05-26 17:03:02 +0530

    updated deployed url

--- server/api/index.js
+++ server/api/index.js
@@ -16,10 +16,16 @@ connectDB();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    // origin: "http://localhost:5173",
-    origin: "https://shopifyclient.vercel.app/",
+    origin: "https://shopifyclient.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
